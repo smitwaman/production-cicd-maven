ae1c0ae0cb5ed9bbf3afc1faabf82a0e7126d2fe
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-03-27 21:05:06 +0530

    Update Jenkins.sh

--- Installation-Scripts/Jenkins.sh
+++ Installation-Scripts/Jenkins.sh
@@ -1,1 +1,6 @@
-
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins

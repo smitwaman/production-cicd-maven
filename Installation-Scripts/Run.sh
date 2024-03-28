7b8724bb0f35dc78700e8a5f0cf120adffa22c68
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-03-28 17:18:02 +0530

    Update Run.sh

--- Installation-Scripts/Run.sh
+++ Installation-Scripts/Run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Run Jenkins container
-docker run -d --name jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins
+docker run -d --name jenkins -p 8080:8080 jenkins/jenkins
 
 # Run SonarQube container
-docker run -d --name sonarqube -p 9000:9000 sonarqube
+docker run -d — name sonar -p 9000:9000 sonarqube:lts-community
 
 # Run Prometheus container
 docker run -d --name prometheus -p 9090:9090 prom/prometheus
@@ -13,13 +13,12 @@ docker run -d --name prometheus -p 9090:9090 prom/prometheus
 docker run -d --name grafana -p 3000:3000 grafana/grafana
 
 # Run Nexus container
-docker run -d --name nexus -p 8081:8081 sonatype/nexus3
+docker run -d — name nexus -p 8081:8081 sonatype/nexus3:latest
 
 # Run PostgreSQL container
 docker run -d --name postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 postgres
 
-# Run Trivy container
-docker run -d --name trivy -v $(pwd):/workdir aquasec/trivy --cache-dir /workdir/cache-dir --skip-update --auto-refresh
+
 
 # Show status of all containers
 docker ps

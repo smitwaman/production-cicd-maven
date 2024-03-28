764e21a1ad2442cb45a4f81c7c85552e5c62c80d
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-03-28 20:41:24 +0530

    Update Nexus.sh

--- Installation-Scripts/Nexus.sh
+++ Installation-Scripts/Nexus.sh
@@ -1,41 +1,50 @@
 
 
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y   
-mkdir -p /opt/nexus/   
-mkdir -p /tmp/nexus/                           
-cd /tmp/nexus/
-NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
-wget $NEXUSURL -O nexus.tar.gz
-sleep 10
-EXTOUT=`tar xzvf nexus.tar.gz`
-NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
-sleep 5
-rm -rf /tmp/nexus/nexus.tar.gz
-cp -r /tmp/nexus/* /opt/nexus/
-sleep 5
-useradd nexus
-chown -R nexus.nexus /opt/nexus 
-cat <<EOT>> /etc/systemd/system/nexus.service
-[Unit]                                                                          
-Description=nexus service                                                       
-After=network.target                                                            
-                                                                  
-[Service]                                                                       
-Type=forking                                                                    
-LimitNOFILE=65536                                                               
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
-User=nexus                                                                      
-Restart=on-abort                                                                
-                                                                  
-[Install]                                                                       
-WantedBy=multi-user.target                                                      
 
-EOT
+# Step 1: Download Nexus
+echo "Downloading Nexus..."
+wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
 
-echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
-systemctl daemon-reload
-systemctl start nexus
-systemctl enable nexus
+# Step 2: Extract the Tarball
+echo "Extracting Nexus tarball..."
+tar -zxvf latest-unix.tar.gz
 
+# Step 3: Move Nexus to desired installation directory (optional)
+# Adjust the destination directory according to your preference
+echo "Moving Nexus to installation directory..."
+mv nexus-*/ /opt/nexus
+
+# Step 4: Set up Nexus as a service (optional)
+# This step assumes you're using systemd for service management
+echo "Setting up Nexus as a service..."
+cat << EOF | sudo tee /etc/systemd/system/nexus.service
+[Unit]
+Description=Nexus Repository Manager
+After=network.target
+
+[Service]
+Type=forking
+LimitNOFILE=65536
+ExecStart=/opt/nexus/bin/nexus start
+ExecStop=/opt/nexus/bin/nexus stop
+User=nexus
+Group=nexus
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 5: Start Nexus service
+echo "Starting Nexus service..."
+sudo systemctl daemon-reload
+sudo systemctl start nexus
+sudo systemctl enable nexus
+
+# Step 6: Verify Nexus installation
+echo "Verifying Nexus installation..."
+sleep 30 # wait for Nexus to start up
+curl -I http://localhost:8081
+
+echo "Nexus installation completed successfully."

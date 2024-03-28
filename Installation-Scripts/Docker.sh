2571b462634e79aec4c0f506e7a9f3f314a6ff8d
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-03-28 20:56:56 +0530

    Update Docker.sh

--- Installation-Scripts/Docker.sh
+++ Installation-Scripts/Docker.sh
@@ -1,6 +1,14 @@
-#!bin/bash
-sudo apt-get update 
-sudo apt-get install docker.io -y 
-sudo usermod -aG docker $USER 
-sudo chmod 777 /var/run/docker.sock  
-sudo docker ps
+#!/bin/bash
+# Update package manager repositories sudo apt-get update
+# Install necessary dependencies
+sudo apt-get install -y ca-certificates curl
+# Create directory for Docker GPG key sudo install -m 0755 -d /etc/apt/keyrings
+# Download Docker's GPG key
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o
+/etc/apt/keyrings/docker.asc
+# Ensure proper permissions for the key sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add Docker repository to Apt sources
+echo "deb [arch=$(dpkg - print-architecture) signed- by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Update package manager repositories sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin

196c30fa7290b2338dfb5fe524fab68bc0d946ea
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-03-28 17:39:37 +0530

    Update Run.sh

--- Installation-Scripts/Run.sh
+++ Installation-Scripts/Run.sh
@@ -4,19 +4,19 @@
 
 
 # Run SonarQube container
-docker run -d — name sonar -p 9000:9000 sonarqube:lts-community
+docker run --name sonar -p 9000:9000 -d sonarqube:lts-community
 
 # Run Prometheus container
-docker run -d --name prometheus -p 9090:9090 prom/prometheus
+docker run --name prometheus -p 9090:9090 -d prom/prometheus
 
 # Run Grafana container
-docker run -d --name grafana -p 3000:3000 grafana/grafana
+docker run --name grafana -p 3000:3000  -d grafana/grafana
 
 # Run Nexus container
-docker run -d — name nexus -p 8081:8081 sonatype/nexus3:latest
+docker run --name nexus -p 8081:8081 -d sonatype/nexus3:latest
 
 # Run PostgreSQL container
-docker run -d --name postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 postgres
+docker run  --name postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d postgres
 
 
 

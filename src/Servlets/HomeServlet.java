572b26f15a3a5fbdd71ab812c0c4d5c30c8a501e
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-03-29 10:49:17 +0530

    Create HomeServlet.java

---
+++ src/Servlets/HomeServlet.java
@@ -0,0 +1,17 @@
+// HomeServlet.java
+package com.yourcompany.blog.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Handle GET request for home page
+        request.getRequestDispatcher("/jsp/home.jsp").forward(request, response);
+    }
+}
